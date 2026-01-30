f603948ee2bf6cb4eb0b8681b8580986e876dfd4
goldenpuffle123 <82688745+goldenpuffle123@users.noreply.github.com>
2026-01-30 10:47:56 -0500

    Add beer videos

--- test.py
+++ test.py
@@ -1,9 +1,26 @@
 from ultralytics import YOLO
 import cv2
+import time
 
-model = YOLO("runs/detect/train4/weights/best.pt")  # trained
+
+TARGET_CLASSES = ["beer"]
+ALPHA = 0.3
+model = YOLO("runs/detect/train4/weights/best.pt", verbose=False)  # trained
 
 cap = cv2.VideoCapture(0)
+no_beer = cv2.VideoCapture("beer_videos/no_beer.mp4")
+yes_beer = cv2.VideoCapture("beer_videos/yes_beer.mp4")
+
+beer_detected = False
+
+def get_overlay_frame(overlay_vid: cv2.VideoCapture):
+    ret, frame = overlay_vid.read()
+    if not ret:
+        overlay_vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        ret, frame = overlay_vid.read()
+    return frame
+
+start = time.time()
 
 while True:
     ret, frame = cap.read()
@@ -17,10 +34,63 @@ while True:
             cls = int(box.cls[0])
             name = model.names[cls]
 
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
-            cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
-            cv2.putText(frame, name, (x1, y1-10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+            if name in TARGET_CLASSES:
+                beer_detected = True
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
+                cv2.putText(frame, name, (x1, y1-10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+                
+    if time.time() - start > 5:
+        beer_detected = True
+                
+    if not beer_detected:
+        overlay = get_overlay_frame(no_beer)
+
+        # Resize overlay to webcam frame
+        if overlay is not None:
+            overlay = cv2.resize(overlay, (frame.shape[1], frame.shape[0]))
+
+            # Alpha blend
+            frame = cv2.addWeighted(
+                frame, 1 - ALPHA,
+                overlay, ALPHA,
+                0
+            )
+
+        cv2.putText(
+            frame,
+            "NO BEER DETECTED",
+            (20, 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 255),
+            3
+        )
+
+    else:
+        overlay = get_overlay_frame(yes_beer)
+
+        # Resize overlay to webcam frame
+        if overlay is not None:
+            overlay = cv2.resize(overlay, (frame.shape[1], frame.shape[0]))
+
+            # Alpha blend
+            frame = cv2.addWeighted(
+                frame, 1 - ALPHA,
+                overlay, ALPHA,
+                0
+            )
+
+        cv2.putText(
+            frame,
+            "BEER DETECTED",
+            (20, 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            3
+        )
 
     cv2.imshow("Beer Can Detection", frame)
     if cv2.waitKey(1) == 27:
